c560a4115dc740718f82a2d6c23121225d46e1ac
haindlb <bernhard.haindl@stud.hcw.ac.at>
2025-12-28 18:39:00 +0100

    Fertig Grundstruktur

---
+++ src/main/java/at/ac/hcw/vmap/App.java
@@ -0,0 +1,25 @@
+package at.ac.hcw.vmap;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+public class App extends Application {
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        FXMLLoader loader = new FXMLLoader(
+                App.class.getResource("/at/ac/hcw/vmap/resources/main_view.fxml")
+        );
+
+        Scene scene = new Scene(loader.load(), 800, 500);
+        stage.setTitle("VMAP");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
--- src/main/java/at/ac/hcw/vmap/ui/VmapUI.java
+++ src/main/java/at/ac/hcw/vmap/ui/VmapUI.java
@@ -1,4 +1,8 @@
 package at.ac.hcw.vmap.ui;
 
+import javafx.stage.Stage;
+
 public class VmapUI {
+    public VmapUI(Stage stage) {
+    }
 }

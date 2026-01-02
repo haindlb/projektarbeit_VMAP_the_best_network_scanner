d145b1f31bbeb3b3bd116a0b145ab09dd61322c0
Manuel Spalt <karottenburger@posteo.eu>
2026-01-02 16:01:48 +0100

    added exception handling on Host class

--- src/main/java/at/ac/hcw/vmap/network/Host.java
+++ src/main/java/at/ac/hcw/vmap/network/Host.java
@@ -11,13 +11,22 @@ public class Host {
     private String name;
     private InetAddress ipAddress;
 
-    public Host (String Address) throws UnknownHostException {
-        this.ipAddress=InetAddress.getByName(Address);
+    public Host (String Address){
+        try {
+            this.ipAddress=InetAddress.getByName(Address);
+        }catch (UnknownHostException e){
+            System.out.println("Invalid Host");
+        }
+
     }
 
-    public Host (String name,String Address) throws UnknownHostException {
+    public Host (String name,String Address) {
         this.name=name;
-        this.ipAddress=InetAddress.getByName(Address);
+        try {
+            this.ipAddress=InetAddress.getByName(Address);
+        }catch (UnknownHostException e){
+            System.out.println("Invalid Host");
+        }
     }
 
     public InetAddress getIpAddress() {
--- src/main/java/at/ac/hcw/vmap/test/TestClass.java
+++ src/main/java/at/ac/hcw/vmap/test/TestClass.java
@@ -6,15 +6,18 @@ import at.ac.hcw.vmap.scanner.IcmpScanner;
 import java.net.UnknownHostException;
 
 public class TestClass {
-    public static void main(String[] args) throws UnknownHostException {
+    public static void main(String[] args){
 
         Host testHost1=new Host("name","192.168.115.59");
 
+        /*
         if(IcmpScanner.scanICMP(testHost1)){
             System.out.println("REACHABLE");
         }else {
             System.out.println("NOT REACHABLE");
         }
+
+         */
 
 
 
